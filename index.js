b7f2a6a72b73c7ce716053b2af4d0c65563dcb99
Oling Emmanuel Benjamin <olingemmanuel5@gmail.com>
2023-09-10 04:13:22 +0300

    finished the post route

--- index.js
+++ index.js
@@ -2,25 +2,29 @@ import express from "express";
 import * as dotenv from "dotenv";
 import cors from "cors";
 import connectDB from "./mongodb/connect.js";
+import dalleRoutes from "./routes/dalleRoutes.js";
+import postRoutes from "./routes/postRoutes.js";
 
 dotenv.config();
 const app = express();
-app.use(cors);
+app.use(cors());
 app.use(express.json({ limit: "50mb" }));
+app.use("/api/v1/post", postRoutes);
+app.use("/api/v1/dalle", dalleRoutes);
 
-app.get("/", async (req, res) => {
+app.get("/", (req, res) => {
   res.send("Hello from DALL-E");
 });
 
-async function startServer() {
+const startServer = async () => {
   try {
-    connectDB(process.env.MONGODB_URL);
-    app.listen(8080, function () {
-      console.log("Server is running at port 8080");
+    await connectDB(process.env.MONGODB_URL);
+    app.listen(3000, () => {
+      console.log("Server is running at port http://localhost:3000");
     });
   } catch (e) {
     console.log(e);
   }
-}
+};
 
 startServer();
